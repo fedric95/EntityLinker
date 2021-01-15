734dac938415664bcb7d18b3bae4c09199b6fcd7
fedric95 <ricciuti.federico@gmail.com>
2021-01-15 05:16:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='WikiSyns',
+    name='EntityLinker',
     version='0.1.0',    
-    packages=['WikiSyns'],
+    packages=['EntityLinker'],
     install_requires=[
         'spacy>=2.3.4'
     ]
