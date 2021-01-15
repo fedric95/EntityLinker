2d0e8409ba732fdb3a65c6fbdad0a7b65a05077c
fedric95 <ricciuti.federico@gmail.com>
2021-01-15 05:20:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',    
     packages=['EntityLinker'],
     install_requires=[
-        'spacy>=2.3.4'
+        'spacy>=2.3.4',
+        'sentence_transformers>=0.4.1'
     ]
 )
